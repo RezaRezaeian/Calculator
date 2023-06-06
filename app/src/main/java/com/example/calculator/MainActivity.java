c6f71a1369238ad080e8300d93ae9b179ba3aa8e
Reza Rezaeian <rreza.rezaeian@gmail.com>
2023-06-06 10:51:42 +0330

    MainActivity changes

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.calculator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    double num1,num2,Result;
+    String name,text1,text2 = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,5 +38,269 @@ public class MainActivity extends AppCompatActivity {
         EditText txtname = findViewById(R.id.txtName);
         EditText txtnum1 = findViewById(R.id.txtNum1);
         EditText txtnum2 = findViewById(R.id.txtNum2);
+
+        txtnum1.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    txtnum1.setInputType(InputType.TYPE_NULL);
+                    txtnum1.setFocusable(true);
+                    btn0.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum1.setText(txtnum1.getText() + "0");
+                        }
+                    });
+                    btn1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum1.setText(txtnum1.getText() + "1");
+                        }
+                    });
+                    btn2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum1.setText(txtnum1.getText() + "2");
+                        }
+                    });
+                    btn3.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum1.setText(txtnum1.getText() + "3");
+                        }
+                    });
+                    btn4.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum1.setText(txtnum1.getText() + "4");
+                        }
+                    });
+                    btn5.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum1.setText(txtnum1.getText() + "5");
+                        }
+                    });
+                    btn6.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum1.setText(txtnum1.getText() + "6");
+                        }
+                    });
+                    btn7.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum1.setText(txtnum1.getText() + "7");
+                        }
+                    });
+                    btn8.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum1.setText(txtnum1.getText() + "8");
+                        }
+                    });
+                    btn9.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum1.setText(txtnum1.getText() + "9");
+                        }
+                    });
+                    btnnoqte.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum1.setText(txtnum1.getText() + ".");
+                        }
+                    });
+                }
+            }
+        });
+
+        txtnum2.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    txtnum2.setInputType(InputType.TYPE_NULL);
+                    txtnum2.setFocusable(true);
+                    btn0.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum2.setText(txtnum2.getText() + "0");
+                        }
+                    });
+                    btn1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum2.setText(txtnum2.getText() + "1");
+                        }
+                    });
+                    btn2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum2.setText(txtnum2.getText() + "2");
+                        }
+                    });
+                    btn3.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum2.setText(txtnum2.getText() + "3");
+                        }
+                    });
+                    btn4.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum2.setText(txtnum2.getText() + "4");
+                        }
+                    });
+                    btn5.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum2.setText(txtnum2.getText() + "5");
+                        }
+                    });
+                    btn6.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum2.setText(txtnum2.getText() + "6");
+                        }
+                    });
+                    btn7.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum2.setText(txtnum2.getText() + "7");
+                        }
+                    });
+                    btn8.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum2.setText(txtnum2.getText() + "8");
+                        }
+                    });
+                    btn9.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum2.setText(txtnum2.getText() + "9");
+                        }
+                    });
+                    btnnoqte.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            txtnum2.setText(txtnum2.getText() + ".");
+                        }
+                    });
+                }
+            }
+        });
+        txtname.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (hasFocus) {
+                    btn0.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                        }
+                    });
+                    btn1.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                        }
+                    });
+                    btn2.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                        }
+                    });
+                    btn3.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                        }
+                    });
+                    btn4.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                        }
+                    });
+                    btn5.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                        }
+                    });
+                    btn6.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                        }
+                    });
+                    btn7.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                        }
+                    });
+                    btn8.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                        }
+                    });
+                    btn9.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                        }
+                    });
+                    btnnoqte.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                        }
+                    });
+                }
+            }
+        });
+        btnjam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text1 = txtnum1.getText().toString();
+                num1 = Integer.parseInt(text1);
+                text2 = txtnum2.getText().toString();
+                num2 = Integer.parseInt(text2);
+                Result = num1+num2;
+            }
+        });
+        btnmenha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text1 = txtnum1.getText().toString();
+                num1 = Integer.parseInt(text1);
+                text2 = txtnum2.getText().toString();
+                num2 = Integer.parseInt(text2);
+                Result = num1-num2;
+            }
+        });
+        btnzarb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text1 = txtnum1.getText().toString();
+                num1 = Integer.parseInt(text1);
+                text2 = txtnum2.getText().toString();
+                num2 = Integer.parseInt(text2);
+                Result = num1*num2;
+            }
+        });
+        btntaqsim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                text1 = txtnum1.getText().toString();
+                num1 = Integer.parseInt(text1);
+                text2 = txtnum2.getText().toString();
+                num2 = Integer.parseInt(text2);
+                Result = num1/num2;
+            }
+        });
+        btnmosavi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                name = txtname.getText().toString();
+                Intent intent =new Intent (MainActivity.this,MainActivity2.class);;
+                intent.putExtra("Result",Result);
+                intent.putExtra("name",name);
+            }
+        });
     }
 }
